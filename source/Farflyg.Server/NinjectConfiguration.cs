a816313f45250c3c85fcac6f3d5ebde37ce0df8d
Victor Blomberg <victor.blomberg@nerven.se>
2015-11-18 21:37:34 +0100

    Implements toggling of fake status updates via AppSetting
    Controls whether to use FakeStatusCyclocityRepository via AppSetting
    Farflyg.Server.NinjectConfiguration.GenerateFakeData

--- source/Farflyg.Server/NinjectConfiguration.cs
+++ source/Farflyg.Server/NinjectConfiguration.cs
@@ -1,3 +1,7 @@
+using System.Configuration;
+
+using Nerven.Assertion;
+
 namespace Farflyg.Server
 {
     using System;
@@ -19,9 +23,24 @@ namespace Farflyg.Server
     public static class NinjectConfiguration
     {
         [PublicAPI]
+        public static string GenerateFakeDataConfigurationKey => typeof (NinjectConfiguration).FullName + ".GenerateFakeData";
+
+        [PublicAPI]
         public static IKernel CreateDefaultKernel()
         {
+            var _generateFakeData = _ReadBoolFromConfiguration(GenerateFakeDataConfigurationKey, false);
+            return CreateDefaultKernel(_generateFakeData);
+        }
+
+        [PublicAPI]
+        public static IKernel CreateDefaultKernel(bool generateFakeData)
+        {
             var _kernel = new StandardKernel();
+
+            _kernel
+                .Bind<CyclocityCredentials>()
+                .ToMethod(_context => CyclocityCredentials.GetFromConfiguration())
+                .InSingletonScope();
 
             _kernel
                 .Bind<Random>()
@@ -37,60 +56,85 @@ namespace Farflyg.Server
                         _worker.StartAsync();
                     });
 
-            var _fakeUpdateSchedule = TimeSpan.FromSeconds(10);
-            _kernel
-                .Bind<ITaskulerScheduleHandle>()
-                .ToMethod(_context => _context.Kernel.Get<ITaskulerWorker>().AddIntervalSchedule(_fakeUpdateSchedule))
-                .WhenInjectedInto<FakeStatusCyclocityRepository>()
-                .InSingletonScope();
+            if (generateFakeData)
+            {
+                var _fakeUpdateSchedule = TimeSpan.FromSeconds(10);
+                _kernel
+                    .Bind<ITaskulerScheduleHandle>()
+                    .ToMethod(_context => _context.Kernel.Get<ITaskulerWorker>().AddIntervalSchedule(_fakeUpdateSchedule))
+                    .WhenInjectedInto<FakeStatusCyclocityRepository>()
+                    .InSingletonScope();
 
-            _kernel
-                .Bind<TimeSpan>()
-                .ToConstant(_fakeUpdateSchedule)
-                .WhenInjectedInto<FakeStatusCyclocityRepository.DefaultFakeGenerator>();
+                _kernel
+                    .Bind<TimeSpan>()
+                    .ToConstant(_fakeUpdateSchedule)
+                    .WhenInjectedInto<FakeStatusCyclocityRepository.DefaultFakeGenerator>();
 
-            _kernel
-                .Bind<FakeStatusCyclocityRepository.IFakeGenerator>()
-                .To<FakeStatusCyclocityRepository.DefaultFakeGenerator>()
-                .InSingletonScope();
+                _kernel
+                    .Bind<FakeStatusCyclocityRepository.IFakeGenerator>()
+                    .To<FakeStatusCyclocityRepository.DefaultFakeGenerator>()
+                    .InSingletonScope();
 
-            _kernel
-                .Bind<CyclocityCredentials>()
-                .ToMethod(_context => CyclocityCredentials.GetFromConfiguration())
-                .InSingletonScope();
+                _kernel
+                    .Bind<ICyclocityRepository>()
+                    .To<FakeStatusCyclocityRepository>()
+                    .InSingletonScope();
 
-            _kernel
-                .Bind<ICyclocityRepository>()
-                .To<CyclocityRepository>()
-                .WhenInjectedInto<CachingCyclocityRepository>();
+                _kernel
+                    .Bind<ICyclocityRepository>()
+                    .To<CyclocityRepository>()
+                    .WhenInjectedInto<FakeStatusCyclocityRepository>();
+            }
+            else
+            {
+                _kernel
+                    .Bind<ICyclocityRepository>()
+                    .To<CachingCyclocityRepository>()
+                    .InSingletonScope();
 
-            _kernel
-                .Bind<ICyclocityRepository>()
-                .To<CachingCyclocityRepository>()
-                .WhenInjectedInto<FakeStatusCyclocityRepository>()
-                .InSingletonScope();
+                _kernel
+                    .Bind<ICyclocityRepository>()
+                    .To<CyclocityRepository>()
+                    .WhenInjectedInto<CachingCyclocityRepository>();
 
-            _kernel
-                .Bind<ICyclocityRepository>()
-                .To<FakeStatusCyclocityRepository>()
-                .InSingletonScope();
+                _kernel
+                    .Bind<CacheItemPolicy>()
+                    .ToMethod(_context => new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(30), })
+                    .WhenInjectedInto<CachingCyclocityRepository>();
 
-            _kernel
-                .Bind<ObjectCache>()
-                .ToConstant(MemoryCache.Default);
-
-            _kernel
-                .Bind<CacheItemPolicy>()
-                .ToMethod(_context => new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(30), });
+                _kernel
+                    .Bind<ObjectCache>()
+                    .ToConstant(MemoryCache.Default)
+                    .WhenInjectedInto<CachingCyclocityRepository>();
+            }
 
             _kernel
                 .Bind<IMainRepository>()
                 .To<MainRepository>();
-            
+
             _kernel.Bind<ISubStartup>().To<Web.ClientAccessPolicy.Startup>();
             _kernel.Bind<ISubStartup>().To<Web.WebApi.Startup>();
 
             return _kernel;
+        }
+
+        private static bool _ReadBoolFromConfiguration(string configurationKey, bool defaultValue)
+        {
+            var _value = ConfigurationManager.AppSettings[configurationKey];
+
+            if (string.Equals(_value, "true", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            if (string.Equals(_value, "false", StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            Must.Assert(string.IsNullOrEmpty(_value), $"Value of configuration setting {configurationKey} is not a valid boolean. Must be empty, 'true' or 'false'.");
+
+            return defaultValue;
         }
     }
 }
